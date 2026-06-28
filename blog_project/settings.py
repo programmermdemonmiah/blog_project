1cc9d32ca81e6be6c98d17d9be9da07558755ef8
Md. Emon Miah <md.emonmiahisthebest@gmail.com>
2026-06-29 00:40:30 +0600

    updated and completed

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,20 @@ SECRET_KEY = 'django-insecure-by@ip4un9&^a143u0n+h3fo3^fo_dkprmpfx-zw+$vn!q(2w5a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+   "https://blog-project-q8i3.onrender.com",
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://blog-project-q8i3.onrender.com",
+]
 
 
 # Application definition
